ad996b1c93655c6d4255c68a60e8980a3b49360d
unknown <bigboy29@freenet.de>
2011-08-30 21:54:11 +0200

    Closes #1; Bluescreen disappears if you press it away

--- jquery.ie6bluescreen.js
+++ jquery.ie6bluescreen.js
@@ -53,44 +53,69 @@
                     color: '#ffffff'
                 }
             }
-
-            // Setup the html
-            $('body').append('<div id="bluescreenWrapper"> \
-                                  <div id="bluescreen"> \
-                                      <h1>Windows</h1> \
-                                      <p>A totally outdated browser software (Internet Explorer 6) was detected.</p> \
-                                      <p>This browser is older than a whole decade and even its creator, Microsoft,</p> \
-                                      <p>says to move off IE6 and say goodbye to this dinosaur.</p> \
-                                      <p>So please <a href="http://www.ie6countdown.com/" title="Microsofts upgrade page">upgrade your browser</a> \
-                                         or install <a href="http://www.google.com/chromeframe" title="Googles Chrome Frame">Chrome Frame</a> into your IE.</p> \
-                                      <p>Press any key to continue <blink>_</blink></p> \
-                                  </div> \
-                            </div>');
-            // Set CSS styles
-            $('#bluescreenWrapper').css(styles.bluescreenWrapper);
-            $('#bluescreen').css(styles.bluescreen);
-            $('#bluescreen h1').css(styles.bluescreenHeading);
-            $('#bluescreen p').css(styles.bluescreenParagraph);
-            $('#bluescreen p:last').css(styles.bluescreenLastParagraph);
-            $('#bluescreen a').css(styles.bluescreenLink);
-            // Reproduce the oldschool <blink> behaviour for IE6
-            $('#bluescreen blink').each(function() {
-                var element = $(this);
-                setInterval(function() {
-                    if ($(element).css('visibility') == 'visible') { // is(':visible') seems to to work here :(
-                        $(element).css('visibility', 'hidden');
-                    } else {
-                        $(element).css('visibility', 'visible');
-                    }
-                }, 500);
-            });
             
-            // If any key is pressed fade out and remove the bluescreen
-            $(document).bind('keypress', function() {
-                $('#bluescreenWrapper').fadeOut('slow', function() {
-                    $('#bluescreenWrapper').remove();
+            // Set cookie name
+            var cookieName = ie6bluescreen + '=';
+            // Split cookies
+            var cookies = document.cookie.split(';');
+            // Get cookie value
+            for (var i = 0; i < cookies.length; i++) {
+                var c = cookies[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1, c.length)
+                };
+                if (c.indexOf(cookieName) == 0) {
+                    var cookieValue = c.substring(cookieName.length, c.length);
+                } else {
+                    var cookieValue = '';
+                }
+            }
+            
+            // If cookie isn't set show bluescreen
+            if (cookieValue != '1') {
+                // Setup the html
+                $('body').append('<div id="bluescreenWrapper"> \
+                                      <div id="bluescreen"> \
+                                          <h1>Windows</h1> \
+                                          <p>A totally outdated browser software (Internet Explorer 6) was detected.</p> \
+                                          <p>This browser is older than a whole decade and even its creator, Microsoft,</p> \
+                                          <p>says to move off IE6 and say goodbye to this dinosaur.</p> \
+                                          <p>So please <a href="http://www.ie6countdown.com/" title="Microsofts upgrade page">upgrade your browser</a> \
+                                             or install <a href="http://www.google.com/chromeframe" title="Googles Chrome Frame">Chrome Frame</a> into your IE.</p> \
+                                          <p>Press any key to continue <blink>_</blink></p> \
+                                      </div> \
+                                </div>');
+                // Set CSS styles
+                $('#bluescreenWrapper').css(styles.bluescreenWrapper);
+                $('#bluescreen').css(styles.bluescreen);
+                $('#bluescreen h1').css(styles.bluescreenHeading);
+                $('#bluescreen p').css(styles.bluescreenParagraph);
+                $('#bluescreen p:last').css(styles.bluescreenLastParagraph);
+                $('#bluescreen a').css(styles.bluescreenLink);
+                // Reproduce the oldschool <blink> behaviour for IE6
+                $('#bluescreen blink').each(function() {
+                    var element = $(this);
+                    setInterval(function() {
+                        if ($(element).css('visibility') == 'visible') { // is(':visible') seems to to work here :(
+                            $(element).css('visibility', 'hidden');
+                        } else {
+                            $(element).css('visibility', 'visible');
+                        }
+                    }, 500);
                 });
-            });
+            
+                // If any key is pressed fade out and remove the bluescreen
+                $(document).bind('keypress', function() {
+                    $('#bluescreenWrapper').fadeOut('slow', function() {
+                        // Calculate the expire date for the cookie
+                        var expire = new Date().getTime() + 3600000 * 24;
+                        // Set cookie
+                        document.cookie = 'ie6bluescreen=1;expires=' + expire.toGMTString() + ';path=/';
+                        // Remove bluescreen wrapper
+                        $('#bluescreenWrapper').remove();
+                    });
+                });
+            }
         }
     };
 })(jQuery);
